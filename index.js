8b568b3854aec3d69c0c47e2aae184c6afadc6d1
Arpit Mahajan <130462117+Arpitmahajan123@users.noreply.github.com>
2025-04-15 20:05:20 +0530

    This Express Code Run Without Moinitoring

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req,res) => {
+    return res.json({ message : "Hello From Server"});
+});
+
+
+app.get("/slow", async (req, res) => { 
+    try { 
+        const timeTaken = await doSomeHeavyTask(); 
+        return res.json({
+            status: "Success", 
+            message: 'Heavy task completed in ${timeTaken}ms', 
+        })
+    }
+    
+    catch (error) { 
+        return res 
+            .status(500) 
+            .json({ status: "Error", error: "Internal Server Error"});
+        } 
+});
+
+app.listen(PORT, () =>
+    console.log(`Express Server Strated at ${PORT}`)
+);
